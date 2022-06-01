b5b6b74cb683eb2c4e5b6ceffa7ed65fc04fa0ab
sdias-code <silviodias.ms@gmail.com>
2022-06-01 02:03:43 -0400

    modificado startup

--- WebAppCadastro/Startup.cs
+++ WebAppCadastro/Startup.cs
@@ -20,15 +20,16 @@ namespace WebAppCadastro
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
             services.AddDbContext<BancoContext>(options => options
             .UseSqlServer(Configuration.GetConnectionString("Database")));
+
             services.AddScoped<IContatoRepository, ContatoRepository>();
         }
 
@@ -45,7 +46,9 @@ namespace WebAppCadastro
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
